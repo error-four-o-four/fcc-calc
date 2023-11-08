c97d5dfaba51016f1e046c5f1427dff582570212
Jens B <jens.bachmayer@gmx.de>
2023-11-08 22:51:06 +0100

    feat: keyboard handler

--- src/calculator/state/context.tsx
+++ src/calculator/state/context.tsx
@@ -1,9 +1,16 @@
 import type { Dispatch, PropsWithChildren } from 'react';
-import { createContext, useReducer } from 'react';
+import { createContext, useEffect, useReducer } from 'react';
+
+import {
+	type DigitValues,
+	type ArithmeticOperators,
+	type FunctionalOperators,
+} from '../config.ts';
 
 import type { CalculatorState } from './state.ts';
 import type { DispatchedAction } from './actions.ts';
 
+import { isValidNumber } from '../../utils/confirm.ts';
 import { initialCalculatorState } from './state.ts';
 import { stateReducer } from './reducer.ts';
 
@@ -17,6 +24,60 @@ export const CalculatorDispatchContext = createContext<
 
 export default function Calculator({ children }: PropsWithChildren) {
 	const [state, dispatch] = useReducer(stateReducer, initialCalculatorState);
+
+	useEffect(() => {
+		const handleKeyPress = (e: KeyboardEvent) => {
+			const { key } = e;
+			if (isValidNumber(e.key)) {
+				dispatch({
+					type: 'digit',
+					payload: {
+						value: key as DigitValues,
+					},
+				});
+			}
+
+			// @todo hack!
+			const mapKeyToArithmetic: {
+				[x: string]: ArithmeticOperators;
+			} = {
+				'+': 'add',
+				'-': 'subtract',
+				'*': 'multiply',
+				'/': 'divide',
+			};
+
+			if (key in mapKeyToArithmetic) {
+				dispatch({
+					type: 'operator',
+					payload: {
+						value: mapKeyToArithmetic[key],
+					},
+				});
+			}
+
+			const mapKeyToFunctional: {
+				[x: string]: FunctionalOperators;
+			} = {
+				'.': 'decimal',
+				Backspace: 'erase',
+				Enter: 'equals',
+				'=': 'equals',
+			};
+
+			if (key in mapKeyToFunctional) {
+				dispatch({
+					type: 'functional',
+					payload: {
+						value: mapKeyToFunctional[key],
+					},
+				});
+			}
+		};
+
+		document.addEventListener('keyup', handleKeyPress);
+		return () => document.removeEventListener('keyup', handleKeyPress);
+	}, []);
 
 	return (
 		<CalculatorStateContext.Provider value={state}>
